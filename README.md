529ed2930a545fef1bc12a1d83ab43383cdd2795
SaraiFerrer <87380666+SaraiFerrer@users.noreply.github.com>
2022-07-31 20:19:31 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Clase-PRO77
+# Localizador-EEI-2
+código de referencia para la c77
---
+++ screens/Home.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    TouchableOpacity,
+    Platform,
+    StatusBar
+} from "react-native";
+
+export default class HomeScreen extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <SafeAreaView style={styles.droidSafeArea} />
+                
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    },
+    routeCard: {
+        flex: 0.25,
+        marginLeft: 50,
+        marginRight: 50,
+        marginTop: 50,
+        borderRadius: 30,
+        backgroundColor: 'white'
+    },
+    titleBar: {
+        flex: 0.15,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    titleText: {
+        fontSize: 40,
+        fontWeight: "bold",
+        color: "white"
+    },
+    routeText: {
+        fontSize: 35,
+        fontWeight: "bold",
+        color: "black",
+        marginTop: 75,
+        paddingLeft: 30
+    },
+    knowMore: {
+        paddingLeft: 30,
+        color: "red",
+        fontSize: 15
+    }
+});
